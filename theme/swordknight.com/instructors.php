6673c63f7c0652cb278df6c52fdb9273ee8cf69c
swordknight <swordknight@ps598747.dreamhostps.com>
2018-08-11 06:59:55 -0700

    add instructors

---
+++ theme/swordknight.com/instructors.php
@@ -0,0 +1,47 @@
+<?php include_once('inc.header.php'); ?>
+		<div id='wrapper' style='margin-top: 120px;'>
+			<section class='information-section'>
+			<style type='text/css'>
+				td.day, td.slot {
+					color: black;
+					background-color: #eee;
+				}
+				td.day {
+					font-weight: bold;
+				}
+				thead th {
+					font-size: 1.2em;
+					background-color: rgb(52, 116, 219);
+					color: white;
+					text-align: left;
+				}
+				td:last-child.overflow {
+					color: #B51414;
+				}
+				td:last-child {
+					color: #144CB5;
+				}
+			</style>
+<?php
+
+	$instructors = Lib::$Lib->Instructors->GetInstructors();
+?>
+			<table class='tabular-information' style='min-width: 280px;'>
+				<thead>
+					<tr>
+						<th>Instructor</th>
+					</tr>
+				</thead>
+				<tbody>
+<?php foreach ($instructors as $k => $instructor) : ?>
+					<tr>
+						<td><a href='instructor-bio?instructor=<?=$instructor['InstructorId']; ?>'><?=$instructor['Name'] ?></a></td>
+					</tr>
+<?php endforeach ; ?>				
+				</tbody>
+			</table>
+
+			</section>
+			<div id='push'></div>
+		</div>
+<?php include_once('inc.footer.php'); ?>
--- theme/swordknight.com/lib/class.Instructors.php
+++ theme/swordknight.com/lib/class.Instructors.php
@@ -7,6 +7,27 @@ class Instructors {
 	
 	}
 	
+  function GetInstructors() {
+    $sql =
+      "select i.instructor_id, i.name 
+        from skbc_instructors i 
+          left join skbc_classes c on i.instructor_id in (c.instructor1_id, c.instructor2_id, c.instructor3_id) 
+        where i.year = :year and c.class_id is not null 
+        group by i.instructor_id";
+    
+    Lib::$Lib->Database->Clear();
+    Lib::$Lib->Database->year = __YEAR__;
+    $results = Lib::$Lib->Database->Query($sql);
+    $instructors = array();
+    if ($results->Size() > 0) do {
+      $instructors[] = array(
+          'InstructorId' => $results->instructor_id,
+          'Name' => $results->name
+        );
+    } while ($results->Next());
+    return $instructors;
+  }
+  
 	function GetInstructor($instructor_id) {
 		$this->instructors->clear();
 		$this->instructors->instructor_id = $instructor_id;
