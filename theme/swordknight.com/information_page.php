54208cf3c3efb7a858633a6c202714f8ca76361a
swordknight <swordknight@ps598747.dreamhostps.com>
2018-08-11 11:31:00 -0700

    correct styling for mobile

--- theme/swordknight.com/information_page.php
+++ theme/swordknight.com/information_page.php
@@ -1,6 +1,6 @@
 <?php include_once('inc.header.php'); ?>
 		<div id='wrapper' style='margin-top: 60px;'>
-			<section class='information-section' style='width: 60vw; margin: 120px auto; text-align: justify;'>
+			<section class='information-section'>
 			
 <?php get_page_content(); ?>
 
